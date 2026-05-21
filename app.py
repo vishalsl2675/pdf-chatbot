7eb6a346fcb38cd3600527506e143a889ab29ce1
Vishal Lashkar <vishalsl2675@gmail.com>
2026-05-21 15:03:52 +0530

    Fixed dependency conflicts

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 # =====================================================
